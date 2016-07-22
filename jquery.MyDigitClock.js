bda80a64cd3c37faf308a3028025776f0653d342
Chris Reynoso <freedommercenary@gmail.com>
2016-07-22 09:07:42 -0700

    Note higher jQuery version in README. Fix incorrect casing of file name in README. Remove unused code. Reformat to jslint standards. Fix multiple syntax issues.

--- jquery.MyDigitClock.js
+++ jquery.MyDigitClock.js
@@ -1,95 +1,92 @@
 /**
- * @author Paul Chan / KF Software House 
+ * @author Chris Reynoso
  * Homepage: http://www.kfsoft.info
+ * Based off original: Paul Chan / KF Software House
+ * Forked from: https://github.com/kfsoft/MyDigitClock-By-Kfsoft.info
  *
- * Version 0.5
- * Copyright (c) 2010 KF Software House
+ * Version 0.5.1
+ * Copyright for portions of project are held by KF Software House, 2010 as part of project MyDigitClock-By-Kfsoft.info.
+ * All other copyright for project are held by Chris Reynoso, 2016.
  *
  * Licensed under the MIT license:
  * http://www.opensource.org/licenses/mit-license.php
  *
  */
-	
-(function($) {
 
-    var _options = {};
-	var _container = {};
+/*jslint this multivar browser */
+(function ($) {
+    "use strict";
+    var _options = {},
+        _container = {};
 
-	jQuery.fn.MyDigitClock = function(options) {
-		var id = $(this).get(0).id;
-		_options[id] = $.extend({}, $.fn.MyDigitClock.defaults, options);
+    jQuery.fn.MyDigitClock = function (options) {
+        var id = $(this).get(0).id,
+            getDD,
+            showClock;
+        _options[id] = $.extend({}, $.fn.MyDigitClock.defaults, options);
 
-		return this.each(function()
-		{
-			_container[id] = $(this);
-			showClock(id);
-		});
-		
-		function showClock(id)
-		{
-			var d = new Date;
-			var h = d.getHours();
-			var m = d.getMinutes();
-			var s = d.getSeconds();
-			var ampm = "";
-			if (_options[id].bAmPm)
-			{
-				if (h>12)
-				{
-					h = h-12;
-					ampm = " PM";
-				}
-				else
-				{
-					ampm = " AM";
-				}
-			}
-			
-			var templateStr = _options[id].timeFormat + ampm;
-			templateStr = templateStr.replace("{HH}", getDD(h));
-			templateStr = templateStr.replace("{MM}", getDD(m));
-			templateStr = templateStr.replace("{SS}", getDD(s));
-		
-			var obj = $("#"+id);
-			obj.css("fontSize", _options[id].fontSize);
-			obj.css("fontFamily", _options[id].fontFamily);
-			obj.css("color", _options[id].fontColor);
-			obj.css("background", _options[id].background);
-			obj.css("fontWeight", _options[id].fontWeight);
-		
-			//change reading
-			obj.html(templateStr)
-			
-			//toggle hands
-			if (_options[id].bShowHeartBeat)
-			{
-				obj.find("#ch1").fadeTo(800, 0.1);
-				obj.find("#ch2").fadeTo(800, 0.1);
-			}
-			setTimeout(function(){showClock(id)}, 1000);
-		}
-		
-		function getDD(num)
-		{
-			return (num>=10)?num:"0"+num;
-		}
-		
-		function refreshClock()
-		{
-			setupClock();
-		}
-	}
-	
-	//default values
-	jQuery.fn.MyDigitClock.defaults = {
-		fontSize: '50px',
-		fontFamily: 'Microsoft JhengHei, Century gothic, Arial',
-		fontColor: '#ff2200',
-		fontWeight: 'bold',
-		background: '#fff',
-		timeFormat: '{HH}<span id="ch1">:</span>{MM}<span id="ch2">:</span>{SS}',
-		bShowHeartBeat: false,
-		bAmPm:false
-	};
+        getDD = function (num) {
+            return (num >= 10)
+                ? num
+                : "0" + num;
+        };
+
+        showClock = function (id) {
+            var d = new Date(),
+                h = d.getHours(),
+                m = d.getMinutes(),
+                s = d.getSeconds(),
+                ampm = "";
+            if (_options[id].bAmPm) {
+                if (h > 12) {
+                    h = h - 12;
+                    ampm = " PM";
+                } else {
+                    ampm = " AM";
+                }
+            }
+
+            var templateStr = _options[id].timeFormat + ampm;
+            templateStr = templateStr.replace("{HH}", getDD(h));
+            templateStr = templateStr.replace("{MM}", getDD(m));
+            templateStr = templateStr.replace("{SS}", getDD(s));
+
+            var obj = $("#" + id);
+            obj.css("fontSize", _options[id].fontSize);
+            obj.css("fontFamily", _options[id].fontFamily);
+            obj.css("color", _options[id].fontColor);
+            obj.css("background", _options[id].background);
+            obj.css("fontWeight", _options[id].fontWeight);
+
+            //change reading
+            obj.html(templateStr);
+
+            //toggle hands
+            if (_options[id].bShowHeartBeat) {
+                obj.find("#ch1").fadeTo(800, 0.1);
+                obj.find("#ch2").fadeTo(800, 0.1);
+            }
+            setTimeout(function () {
+                showClock(id);
+            }, 1000);
+        };
+
+        return this.each(function () {
+            _container[id] = $(this);
+            showClock(id);
+        });
+    };
+
+    //default values
+    jQuery.fn.MyDigitClock.defaults = {
+        background: "#fff",
+        bAmPm: false,
+        bShowHeartBeat: false,
+        fontColor: "#ff2200",
+        fontFamily: "Open Sans, Helvetica, Arial, Times",
+        fontSize: "50px",
+        fontWeight: "bold",
+        timeFormat: "{HH}<span id=\"ch1\">:</span>{MM}<span id=\"ch2\">:</span>{SS}"
+    };
 
 })(jQuery);
